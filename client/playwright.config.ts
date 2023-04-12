952c592f7cf9fdd816eae2edbb711a32f332feaa
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-04-12 22:57:09 +1200

    ci: working on playwright tests

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -1,21 +1,54 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	testDir: './tests',
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
 	workers: process.env.CI ? 1 : undefined,
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173,
-		reuseExistingServer: !process.env.CI
-	},
-	testDir: 'tests',
+	reporter: 'html',
 	use: {
-		baseURL: 'http://localhost:4173/',
+		baseURL: 'http://localhost:4173',
 		trace: 'on-first-retry'
 	},
-	expect: { timeout: 100000 }
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		},
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] }
+		}
+		/* Test against mobile viewports. */
+		// {
+		//   name: 'Mobile Chrome',
+		//   use: { ...devices['Pixel 5'] },
+		// },
+		// {
+		//   name: 'Mobile Safari',
+		//   use: { ...devices['iPhone 12'] },
+		// },
+
+		/* Test against branded browsers. */
+		// {
+		//   name: 'Microsoft Edge',
+		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+		// },
+		// {
+		//   name: 'Google Chrome',
+		//   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
+		// },
+	],
+	webServer: {
+		command: 'npm run build && npm run preview',
+		url: 'http://localhost:4173',
+		reuseExistingServer: !process.env.CI
+	}
 };
 
 export default config;
