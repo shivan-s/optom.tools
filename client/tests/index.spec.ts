987f3b8591479f62c8c6376d8f4ececcd553fb03
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-04-13 21:09:50 +1200

    feat: WIP

--- client/tests/index.spec.ts
+++ client/tests/index.spec.ts
@@ -5,24 +5,15 @@ test.describe('e2e', () => {
 		await page.goto('/');
 	});
 
-	test('page navigates to home', async ({ page }) => {
+	test('page navigation', async ({ page }) => {
 		await page.getByRole('link', { name: 'Home' }).click();
 		expect(
 			page.getByRole('heading', { name: 'Optometry Resources' })
 		).toBeVisible();
-	});
-
-	test('page navigates to calculators', async ({ page }) => {
 		await page.getByRole('link', { name: 'Calculators' }).click();
 		expect(page.getByRole('heading', { name: 'Calculators' })).toBeVisible();
-	});
-
-	test('page navigates to practitioners', async ({ page }) => {
 		await page.getByRole('link', { name: 'Practitioners' }).click();
 		expect(page.getByRole('heading', { name: 'Practitioners' })).toBeVisible();
-	});
-
-	test('page navigates to clinics', async ({ page }) => {
 		await page.getByRole('link', { name: 'Clinics' }).click();
 		expect(page.getByRole('heading', { name: 'Clinics' })).toBeVisible();
 	});
