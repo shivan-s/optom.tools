ed037c7c0452d32b231cd5a274433764cf1184bb
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-04-18 23:02:33 +1200

    feat: WIP

---
+++ cms/src/api/clinic/routes/clinic.ts
@@ -0,0 +1,7 @@
+/**
+ * clinic router
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreRouter("api::clinic.clinic");
---
+++ cms/src/api/practitioner/controllers/practitioner.ts
@@ -0,0 +1,7 @@
+/**
+ * practitioner controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreController("api::practitioner.practitioner");
