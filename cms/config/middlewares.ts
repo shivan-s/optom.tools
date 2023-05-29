e6821a46266beff20906013a49b3eafe57da045d
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-05-29 21:26:07 +1200

    fix: deployment

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -1,20 +1,22 @@
-export default [
-  "strapi::errors",
-  "strapi::security",
-  "strapi::poweredBy",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      methods: ["GET", "POST", "OPTIONS"],
-      headers: "*",
-      origin: ["https://optom-resources.onrender.com"],
+export default ({ env }) => {
+  [
+    "strapi::errors",
+    "strapi::security",
+    "strapi::poweredBy",
+    {
+      name: "strapi::cors",
+      config: {
+        enabled: true,
+        methods: ["GET", "POST", "OPTIONS"],
+        headers: "*",
+        origin: env("CORS_WHITELIST").split(","),
+      },
     },
-  },
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-];
+    "strapi::logger",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+  ];
+};
