4e96fa6ffb600dd4ea2aec2f3cdc4759c809ae15
Shivan Sivakumaran <me@shivan.xyz>
2024-09-14 17:18:16 +1200

    ci: wip15

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 
 The goal of [optom.tools](optom.tools) is to provide easy to use calculators for anyone in the optics space.
 
+## E2E Tests
+
+These tests must be run on a macos machine. This is for the screenshots.
+
 ## Resources
 
 - [Blog post](https://shivan.xyz/projects/optom-tools/)
--- tests/test.ts
+++ tests/test.ts
@@ -1,9 +1,16 @@
 import { expect, test } from '@playwright/test';
 
-test.describe('snapshots', () => {
-	test('home page', async ({ page }) => {
-		await page.goto('/');
-		await page.screenshot({ path: 'home.png', fullPage: true });
-		await expect(page).toHaveScreenshot('home.png');
+test.describe('Test suite', () => {
+	const links = [
+		{ name: 'home', link: '/' },
+		{ name: 'back-vertex-power', link: '/back-vertex-power' },
+		{ name: 'cylindrical-transposition', link: '/cylindrical-transposition' },
+		{ name: 'minimum-blank-size', link: '/minimum-blank-size' }
+	];
+	links.forEach(({ name, link }) => {
+		test(`${link} page`, async ({ page }) => {
+			await page.goto(link);
+			await expect(page).toHaveScreenshot(`${name}.png`);
+		});
 	});
 });
