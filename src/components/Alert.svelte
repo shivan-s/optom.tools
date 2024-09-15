d555a9ab2f577f0633972904e6ebe810b1691bba
Shivan Sivakumaran <me@shivan.xyz>
2024-09-15 21:07:14 +1200

    feat: alert component Closes #37

---
+++ src/components/Alert.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { sineIn } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	interface $$Props extends HTMLAttributes<HTMLDivElement> {}
+</script>
+
+<div role="alert" in:fade={{ easing: sineIn }} {...$$props}>
+	<slot />
+</div>
+
+<style>
+	div {
+		display: flex;
+		justify-content: center;
+		font-size: 1.5rem;
+		margin: 1rem 0rem;
+		padding: 1rem;
+		width: 100%;
+		border-radius: 0.25rem;
+		border: 2px solid var(--primary);
+		box-shadow: 0.25rem 0.25rem 0.5rem var(--primary);
+		font-family: 'Optician Sans', sans-serif;
+	}
+</style>
