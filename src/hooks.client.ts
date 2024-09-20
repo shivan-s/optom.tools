08a3561ee26f5d63deb4f7ff3665c5421b7d4e51
Shivan Sivakumaran <me@shivan.xyz>
2024-09-20 22:19:41 +1200

    feat: adding sentry and health checks Closes #42

---
+++ src/hooks.client.ts
@@ -0,0 +1,22 @@
+import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: 'https://f14d06ea23672f4920154a6bd628212d@o1039828.ingest.us.sentry.io/4507984676454400',
+
+	tracesSampleRate: 1.0,
+
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
+
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
+
+	// If you don't want to use Session Replay, just remove the line below:
+	integrations: [replayIntegration()]
+});
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
